1595a7619ba432e816c7d6eb069b96da70b3043f
Kelechi Igbokwe <kells@Kelechis-MacBook-Pro.local>
2018-06-23 00:44:21 +0200

    first commit

---
+++ run.py
@@ -0,0 +1,21 @@
+from blog import app
+#app.config['MONGO_DBNAME'] = 'projectfinder'
+#app.config['MONGO_URI'] = 'mongodb://localhost:27017/projectfinder'
+
+
+#mongo = PyMongo(app)
+
+#client = MongoClient('localhost:27017')
+#db = client.itproject
+
+#projects = mongo.db.itproject
+#project_list = db.find()
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
